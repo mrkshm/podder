274ee00edd155f9976ffd241d82820fbe799e100
mrks <m.heumann@legendaryplays.com>
2023-10-27 12:03:49 +0200

    wip: auth

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,11 @@ import express from "express";
 import "dotenv/config";
 import "./db";
 import { PORT } from "@/utils/variables";
-
+import authRouter from './routers/auth';
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
